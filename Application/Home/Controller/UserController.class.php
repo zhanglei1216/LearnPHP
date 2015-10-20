f215918756aeb930e1b731691b9f36bdc91d5c18
zhanglei <zhanglei1216@163.com>
2015-10-20 18:15:30 +0800

    提交今天更改

--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -5,12 +5,13 @@
  * Date: 15/10/17
  * Time: 下午1:55
  */
-
 namespace Home\Controller;
 use Think\Controller;
 use Think\Model;
 
 class UserController extends Controller {
+
+
     public  function  create(){
 //       $user = M('User');
 //        var_dump($user->create($_POST, Model::MODEL_INSERT));
@@ -45,9 +46,12 @@ class UserController extends Controller {
 
 //        var_dump($user->add($data));
     }
+
+
+    /*数据读取*/
     public function select(){
         $user = M('User');
-//        var_dump($user->select());r
+//        var_dump($user->select());
         //获取第一条所有数据
 //        var_dump($user->find());
         //获取第一条user字段值
@@ -55,13 +59,162 @@ class UserController extends Controller {
         //获取所有user
 //        var_dump($user->getField('user', true));
         //传递多个字段获取所有 重复的会被去掉 第一个字段当key
-        var_dump($user->getField('user,email'));
+//        var_dump($user->getField('user,email'));
         //传递多个字段获取所有
 //        var_dump($user->getField('id,user,email'));
         //输出字符创格式的数组字符串中不同的字段值用制定的分隔符隔开
 //        var_dump($user->getField('id,user,email', ':'));
 
+
+    }
+    /**
+     * 数据更新
+     */
+    public function save(){
+        $user = M('User');
+        //连贯操作update
+//        $data['user'] = '樱桃大丸子';
+//        $data['email'] = '随便输入';
+//        $map['id'] = 1;
+//        $user->where($map)->save($data);
+
+        //非连贯操作update
+//        $data['user'] = '樱桃';
+//        $data['email'] = '326023094@qq.com';
+//        $data['id'] = 1;
+//        $user->save($data);
+
+        //自动获取表单update
+//        $user->create();
+//        $user->save();
+
+        //更改一个字段
+//        $map['id'] = 1;
+//        $user->where($map)->setField('user', '新新');
+
+        //字段累加累减
+//        $map['id'] = 1;
+//        echo $user->where($map)->setInc('cout', 2);
+//        echo $user->where($map)->setDec('cout', 1);
+    }
+    public function delete(){
+        $user = M('User');
+
+        //直接删除参数为主键
+//        $user->delete(2);
+
+        //连贯操作按住键删除
+//        $map['id'] = 1;
+//        $user->where($map)->delete();
+
+        //批量删除
+//        $user->delete('2,4,5');
+
+
+        $map['cout'] = 0;
+        $user->where($map)->order(array('date'=>'DESC'))->limit(2)->delete();
+
     }
 
+    public function ar(){
+        $user = M('user');
+
+//        $user->user = '添加';
+//        $user->email = '添加email';
+//        $user->cout = '5';
+//        $user->date = date('Y-m-d H:i:s');
+//        $user->add();
+
+        //按住键查找
+//        var_dump($user->find(2));
+
+        //按user值查找
+//        $user->getByUser('新新');
+//        echo $user->email;
+
+        //直接按多个主键查找
+//        var_dump( $user->select('1,2,3'));
+
+        //修改当前找到的数据
+//        $user->find(1);
+//        $user->user = 'userss';
+//        $user->save();
+
+
+        //删除当前找到的数据
+//        $user->find(3);
+//        $user->delete();
+
+        //按一个或多个主键删除
+//        $user->delete(2);
+    }
+
+    public function model(){
+        //字段映射将 protected $_map = array('xiangming'=>'user', 'youxiang' => 'email');
+        $user = D('User');
+//        $user->scope()->sql1()->select();
+//        var_dump($user->scope()->sql2()->select());
+//        var_dump($user->scope()->select());
+//        var_dump($user->scope('sql1')->select());
+
+
+    }
+
+
+
+    public function valid(){
+        header('Content-Type:text/html; charset=utf-8');
+//        header("Content-type:text/html;charset=utf-8");
+
+        /*静态验证*/
+//        $user = D('user');
+//        $data['user'] = 'zx'] = 'lisi';
+//        if($user->create($data)){
+//            echo '验证通过';
+//        }else{
+//            $this->ajaxReturn($user->getError(),'JSON');
+//        }
+
+
+       /*动态验证*/
+
+
+        $rule = array(
+//        array('user', 'require', '用户名不得为空！', 0, 'regex', 3),
+            array('email', 'email', '邮箱格式不正确', 0, 'regex', 3),
+//        array('user', 'url', 'url格式不合法'),
+//        array('user', 'currency', '货币格式不正确！'),
+//        array('user', 'zip', '邮政编码必须是六位正整数！'),
+//        array('user', 'number', '必须是正整数！'),
+//        array('user', 'integer', '必须是整数！'),
+//        array('user', 'double', '必须是浮点数'),
+//        array('user', 'english', '必须是纯英文！'),
+//        array('user', '/^[0-9]{3,6}$/', '必须是3-6位纯数字'),
+//        array('user', 'zhanglei', '传递过来的值不相等!', 0, 'equal'),
+//        array('user', 'zhanglei', '传递过来的值不能等于zhanglei', 0, 'notequal'),
+//        array('user', 'name', '两个值不相等！', 0, 'confirm'),
+//        array('user', array(1,3,4), '不在指定范围内！', 0, 'in'),
+//        array('user', 'zhangsan,lisi,wangwu', '不允许在制定范围内！', 0, 'notin'),
+//        array('user', '3', '不得小于三位!', 0, 'length'),
+//        array('user', '3,6', '不得小于三位不得大于六位！', 0, 'length'),
+//        array('user', array(3, 5), '必须是3-5之间的数字！', 0, 'between'),
+//        array('user', array(3, 5), '不得是3-5之间的数字！', 0, 'notbetween'),
+//        array('user', '2015-1-1,2015-10-10', '表单提交不在有效期内！', 0, 'expire'),
+//        array('user', '128.0.0.1', '您的ip没有被允许！', 0, 'ip_allow'),
+//        array('user', '127.0.0.1', '您的ip被禁止！', 0, 'ip_deny'),
+        array('user', 'checkLength', '用户名长度必须在3-5之间', 0, 'callback'),
+//            array('user', 'checkLength', '用户名长度必须在3-5之间', 0, 'function')
+        );
+
+        $user = D('User');
+        $data['user']  = 'lisi';
+        $data['email'] = 'fdsa';
+
+        if($user->validate($rule)->create($data)){
+            echo '验证通过';
+        }else{
+            print_r($user->getError());
+        }
+    }
 
 } 
--- Application/Home/Model/UserModel.class.php
+++ Application/Home/Model/UserModel.class.php
@@ -23,14 +23,93 @@ class UserModel extends Model {
     //更改数据库名
 //    protected  $dbName = 'mysql';
 
+//映射字段
+    protected $_map = array(
+        'xiangming'=>'user',
+        'youxiang' => 'email'
+    );
+
+//查找字段
+    protected $_scope = array(
+        'sql1'=>array(
+            'where'=>array(
+                'id'=>1
+            )
+        ),
+        'sql2'=>array(
+            'order'=>array(
+                'date'=>'DESC'
+            ),
+            'limit'=>2,
+        ),
+
+        'default'=>array(
+            'where'=>array(
+                'id'=>2
+            )
+        )
+    );
+
+    /**
+     * 第一个为字段名
+     * 第二个为 验证规则 require（字段必填）、email（邮箱格式）、url（url格式）、currency（货币）、number（正整数）、integer（整数）、double（浮点数）
+     * zip（邮政编码）、english（英文）默认采用的规则是正则表达式（regex）
+     * 第三个为错误提示
+     * 第四个为验证条件 1.self::EXITS_VALIDATE 或 0， 表示字段存在就验证（默认）；
+     *                2.self::MUST_VALIDATE 或 1，表示必须验证；
+     *                3.self::VALUE_VALIDATE 或 2，表示值不为空得时候验证
+     * 第五个为附加规则 regex 正则验证 function 函数验证
+     * 第六个为验证时间 1.self::MODEL_INSERT 或 1 新增数据时验证
+     *                2.self::MODEL_UPDATE 或 2 编辑数据时验证
+     *                3.self::MODEL_BOTH 或 3 全部情况下验证（默认）
+     *（后三个可以不写）
+     */
+//    protected $_validate = array(
+////        array('user', 'require', '用户名不得为空！', 0, 'regex', 3),
+//        array('email', 'email', '邮箱格式不正确', 0, 'regex', 3),
+////        array('user', 'url', 'url格式不合法'),
+////        array('user', 'currency', '货币格式不正确！'),
+////        array('user', 'zip', '邮政编码必须是六位正整数！'),
+////        array('user', 'number', '必须是正整数！'),
+////        array('user', 'integer', '必须是整数！'),
+////        array('user', 'double', '必须是浮点数'),
+////        array('user', 'english', '必须是纯英文！'),
+////        array('user', '/^[0-9]{3,6}$/', '必须是3-6位纯数字'),
+////        array('user', 'zhanglei', '传递过来的值不相等!', 0, 'equal'),
+////        array('user', 'zhanglei', '传递过来的值不能等于zhanglei', 0, 'notequal'),
+////        array('user', 'name', '两个值不相等！', 0, 'confirm'),
+////        array('user', array(1,3,4), '不在指定范围内！', 0, 'in'),
+////        array('user', 'zhangsan,lisi,wangwu', '不允许在制定范围内！', 0, 'notin'),
+////        array('user', '3', '不得小于三位!', 0, 'length'),
+////        array('user', '3,6', '不得小于三位不得大于六位！', 0, 'length'),
+////        array('user', array(3, 5), '必须是3-5之间的数字！', 0, 'between'),
+////        array('user', array(3, 5), '不得是3-5之间的数字！', 0, 'notbetween'),
+////        array('user', '2015-1-1,2015-10-10', '表单提交不在有效期内！', 0, 'expire'),
+////        array('user', '128.0.0.1', '您的ip没有被允许！', 0, 'ip_allow'),
+////        array('user', '127.0.0.1', '您的ip被禁止！', 0, 'ip_deny'),
+//        array('user', 'checkLength', '用户名长度必须在3-5之间', 0, 'callback'),
+////        array('user', 'checkLength', '用户名长度必须在3-5之间', 0, 'function')
+//    );
+
+    protected $patchValidate = true;
 
     public function __construct(){
         parent::__construct();
-        echo "/home";
     }
 
+    public  function checkLength($str, $min, $max){
+        //匹配成数组
+//        preg_match_all('/./u', $str, $matches);
+//        $len = count($matches[0]);
+        $len = strlen($str);
+        if($len >= 3 && $len <= 5){
+            return true;
+        }else{
+            return false;
+        }
+    }
 
-    //在模型中限制字段
-    protected $insertFields = 'user';
-    protected $updateFields = 'user';
+//    //在模型中限制字段
+//    protected $insertFields = 'user';
+//    protected $updateFields = 'user';
 } 
